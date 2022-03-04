3c805eec20eb3049ef46a6b63dd3a9a936825809
Dynastiy <emmanuel00michael@gmail.com>
2022-03-04 13:12:21 +0100

    completed update details and view verified merchants

--- pages/update_kyc_details.vue
+++ pages/update_kyc_details.vue
@@ -1,48 +1,94 @@
 <template>
   <div>
     <div class="update_kyc_details">
-      <h1 class="text-center text-white">KYC Form</h1>
-      <div class="kyc_form_info">
-        <div class="form_wrap mt-3">
-          <form action="" @submit.prevent="onSubmit()">
-            <div class="">
-              <div class="form-group mx-2 mt-2">
-                <v-text-field
-                  label="Ref Code"
-                  v-model="kyc_info.reference_number"
-                  placeholder="Enter Ref Code"
-                  hint="Input Reference Code Sent To Your Mail"
-                  required
-                />
-              </div>
-              <div class="state_wrap">
-                <select class="custom-select custom-select-sm">
-                  <option selected>Open this select menu</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-              </div>
-
-              <div class="state_wrap mt-4">
-                <select class="custom-select custom-select-sm">
-                  <option selected>Open this select menu</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-              </div>
-
-              <div class="submit_button_wrapper mt-4">
-                <v-btn
-                  class="success bg-success apply-to-join-btn"
-                  type="submit"
-                  :loading="loading"
-                  >Submit</v-btn
-                >
-              </div>
+      <div class="px-3 mb-3" v-if="submitted">
+        <div class="verified" >
+          <div>
+            <div class="icon-div">
+              <ion-icon name="checkmark-outline"></ion-icon>
             </div>
-          </form>
+            <h3 class="text-white">Update Succesful!</h3>
+            <h3 >
+              You can now Exit the browser</h3>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <h1 class="text-center text-white mt-4">Update KYC Details</h1>
+        <div class="kyc_form_info">
+          <div class="form_wrap p-3">
+            <form action="" @submit.prevent="onSubmit()">
+              <div class="">
+                <p class="small mt-3" :class="[msg]">{{ msg }}</p>
+                <div class="form-group mt-2">
+                  <v-text-field
+                    label="Ref Code"
+                    v-model="kyc_info.reference_number"
+                    placeholder="Enter Ref Code"
+                    hint="Input Reference Code Sent To Your Mail"
+                    required
+                  />
+                  <input type="button" class="check__kyc" @click="checkKYC" value="Confirm Reference">
+                </div>
+                <div v-if="other_details">
+                  <div class="state_wrap">
+                    <label for="">Select State</label>
+                    <select
+                      class="custom-select custom-select-sm"
+                      id="selectState"
+                      v-model="kyc_info.state"
+                      @change="getState()"
+                    >
+                      <option value="---">---</option>
+                      <option
+                        v-for="item in states"
+                        :key="item.id"
+                        :value="item.name"
+                        :id="item.alias"
+                      >
+                        {{ item.name }}
+                      </option>
+                    </select>
+                  </div>
+
+                  <div class="state_wrap mt-4">
+                    <label for="">Select City</label>
+                    <select
+                      class="custom-select custom-select-sm"
+                      v-model="kyc_info.city"
+                    >
+                      <option value="---">---</option>
+                      <option
+                        :value="item.name"
+                        v-for="item in city"
+                        :key="item.id"
+                      >
+                        {{ item.name }}
+                      </option>
+                    </select>
+                  </div>
+
+                  <div class="form-group mt-3">
+                    <v-text-field
+                      label="Valid Phone Number"
+                      v-model="kyc_info.phone_number"
+                      placeholder="Enter Phone Number"
+                      required
+                    />
+                  </div>
+
+                  <div class="submit_button_wrapper mt-4">
+                    <v-btn
+                      class="success bg-success apply-to-join-btn"
+                      type="submit"
+                      :loading="loading"
+                      >Submit</v-btn
+                    >
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
@@ -54,15 +100,80 @@ export default {
   data() {
     return {
       loading: false,
+      other_details: false,
+      submitted: false,
       kyc_info: {
         reference_number: "",
         state: "",
         city: "",
+        phone_number: "",
       },
-
-      state: {},
-      city: {},
+      msg: "",
+      states: [],
+      city: [],
+      selState: "",
     };
+  },
+  methods: {
+    async checkKYC() {
+      let payload = {
+        reference_number: this.kyc_info.reference_number,
+      };
+      this.$axios
+        .post("api/kyc/verify-reference", payload)
+        .then((res) => {
+          console.log(res);
+          this.msg = res.data.message;
+          if (res.data.message === "Reference Number is valid") {
+            this.other_details = true;
+          } else {
+            this.other_details = false;
+          }
+          // this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+          // this.loading = false;
+        });
+    },
+    async onSubmit() {
+      this.$axios
+        .post("api/kyc/update", this.kyc_info)
+        .then((res) => {
+          // this.$router.push("/notification");
+          this.kyc_info = {};
+          console.log(res.data);
+          // this.loading = false;
+          this.submitted = true;
+        })
+        .catch((error) => {
+          console.log(error);
+          // this.loading = false;
+        });
+    },
+    getState() {
+      var stateOptions = document.getElementById("selectState");
+
+      var selOption = stateOptions.options[stateOptions.selectedIndex].id;
+      console.log(selOption);
+      this.selState = selOption;
+      this.getlgas();
+    },
+    async getStates() {
+      let res = await this.$axios.get("https://locus.fkkas.com/api/states");
+      this.states = res.data.data;
+      console.log(this.states);
+    },
+    async getlgas() {
+      let res = await this.$axios.get(
+        `https://locus.fkkas.com/api/regions/${this.selState}`
+      );
+      this.city = res.data.data;
+      console.log(this.city);
+    },
+  },
+  async created() {
+    this.getStates();
   },
 };
 </script>
@@ -103,7 +214,53 @@ export default {
 .update_kyc_details .state_wrap .custom-select:focus-within {
   border-color: #30303025;
 }
-
+.check__kyc {
+  background: #035303;
+  color: #fff;
+  padding: 0.2rem 1rem;
+  font-size: 0.8rem;
+  border-radius: 5px;
+}
+.valid {
+  color: #035303;
+}
+.Invalid {
+  color: #ff0000;
+}
+.verified {
+  display: flex;
+  /* width: 30%; */
+  height: 100vh;
+  align-items: center; 
+  justify-content: center;
+  /* position: absolute;
+  top: 32%;
+  left: 45%; */
+  padding: 0.3rem 0.8rem;
+  gap: 10px;
+}
+.verified ion-icon {
+  color: #aff7af;
+  font-weight: bold;
+  font-size: 1.1rem;
+  background-color: #aff7af3a;
+  border: 1px solid #aff7af3a;
+  border-radius: 50%;
+  height: 8rem;
+  width: 8rem;
+}
+.verified .icon-div {
+  text-align: center;
+  margin: 0 auto;
+}
+.verified h3{
+  margin: 0;
+  /* font-size: 0.8rem; */
+  font-weight: bold;
+  /* margin-top: -1.3rem; */
+  color: #aff7af;
+  text-align: center;
+}
 @media (max-width: 768px) {
   .update_kyc_details {
     padding: 1rem;
--- pages/verified_merchants.vue
+++ pages/verified_merchants.vue
@@ -3,63 +3,198 @@
     <div class="verified_merchant_wrap">
       <div class="verification_content">
         <div class="text-center py-3">
-          <h1>Verified Merchants</h1>
+          <h1 class="font-weight-bold">SZC Verified Merchants</h1>
+          <hr style="width:20%; margin: 0 auto">
         </div>
         <div class="table-responsive">
-          <v-simple-table fixed-header height="300px">
+           <v-simple-table fixed-header height="500px">
             <template v-slot:default>
-              <thead>
-                <tr class="">
-                  <th class="text-left th_color">Ref No</th>
-                  <th class="text-left th_color">Name</th>
-                  <th class="text-left th_color">Email</th>
-                  <th class="text-left th_color">Phone No</th>
-                  <th class="text-left th_color">Address</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="mt-2">
-                  <td>ART4253VCFS</td>
-                  <td>Excel Graphixz</td>
-                  <td>excelgraphixz@gmail.com</td>
-                  <td>09088725534</td>
-                  <td>no 21 ekpri nsukara off nwaniba</td>
-                </tr>
-                <tr class="py-2">
-                  <td>RTRFW4566GD</td>
-                  <td>Ime Akpan</td>
-                  <td>ime@gmail.com</td>
-                  <td>070227536576</td>
-                  <td>no 40 ebenezer street</td>
-                </tr>
-                <tr class="py-2">
-                  <td>sURFW4556GD</td>
-                  <td>Ekpe Ekpad</td>
-                  <td>ekpe@gmail.com</td>
-                  <td>090772511176</td>
-                  <td>no 10 imeh uwah street</td>
-                </tr>
-              </tbody>
+            <thead>
+              <tr class="">
+                <td class="text-left th_color"></td>
+                <th class="text-left th_color">Reference No</th>
+                <th class="text-left th_color">Name</th>
+                <th class="text-left th_color">Email</th>
+                <th class="text-left th_color">Phone No</th>
+                <th class="text-left th_color">Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="mt-2" v-for="item in dataArr" :key="item.id" @click="viewMore(item)">
+                <td>
+                  <span v-for="doc in item.documents" :key="doc.id"> 
+                  <img v-if="doc.name === 'Passport Photo' " :src="baseurl + doc.file" alt=""  class="image__pp"> </span>
+                </td>
+                <!-- <td > {{ doc.name }} </td> -->
+                <td class="text-uppercase">{{ item.reference_number }}</td>
+                <td> {{ item.name }} </td>
+                <td> {{ item.email }} </td>
+                <td>  <span v-if="item.phone_number === null ">null</span> <span v-else> {{ item.phone_number }} </span>  </td>
+                <td> <span v-if="item.state === null ">null</span> <span v-else>{{ item.city }}, {{ item.state }}</span> </td>
+              </tr>
+            </tbody>
             </template>
-          </v-simple-table>
+           </v-simple-table>
+            <nav>
+          <ul
+            class="pagination pagination-md d-flex justify-content-center align-items-center mt-5" style="gap:20px"
+          >
+            <li class="page-item" :class="{ disabled: dataObj.total === 0 }">
+              <a
+                href="javascript:void(0)"
+                class="page-link"
+                @click="next(dataObj.current_page - 1)"
+              >
+                &laquo;
+              </a>
+            </li>
+            <li>
+               Showing <span> {{ dataObj.from }} - {{ dataObj.to }} </span> of <span> {{ dataObj.total }} </span>
+            </li>
+            <!-- <li
+              class="page-item"
+              v-for="page in dataObj.last_page"
+              :key="page"
+              :class="{active: dataObj.current_page === page }"
+            >
+              <a
+                href="javascript:void(0)"
+                @click.prevent="next(page)"
+                class="page-link"
+              >
+                {{ page}}
+              </a>
+            </li> -->
+            <li
+              class="page-item"
+              :class="{ disabled: dataObj.current_page === dataObj.last_page }"
+            >
+              <a
+                href="javascript:void(0)"
+                class="page-link"
+                @click="next(dataObj.current_page + 1)"
+              >
+                &raquo;
+              </a>
+            </li>
+          </ul>
+        </nav>
         </div>
       </div>
+    </div>
+
+
+    <div class="merchant-card-container" v-show="merchant_card">
+      <div class="merchant__card">
+        <div class="d-flex  align-items-center justify-content-between pt-3 px-3">
+          <h5 class="font-weight-bold m-0"> {{merchant.name}} </h5>
+        <ion-icon name="close-outline" @click="merchant_card  = !merchant_card "></ion-icon>
+        </div>
+         <div class="px-3  mb-3">
+           <div class="verified">
+          <ion-icon name="checkmark-outline"></ion-icon>
+        <h5>Verified</h5>
+      </div>
+         </div>
+      <div class=" d-md-flex">
+        <div class="left__side text-center  p-3">
+        <span v-for="doc in merchant.documents" :key="doc.id"> 
+              <img v-if="doc.name === 'Passport Photo' " :src="baseurl + doc.file" alt=""  class="image__pp2"> </span>
+          <div class="icons">
+            <ion-icon name="call-outline"></ion-icon>
+            <a href="tel:merchant.phone_number" class=""> <span v-if="merchant.phone_number === null ">null</span> <span v-else> {{ merchant.phone_number }} </span>   </a>
+          </div>
+          <div class="icons">
+            <ion-icon name="mail-outline"></ion-icon>
+            <a class="" href="mailto:merchant.email"> {{ merchant.email }} </a>
+          </div>
+        
+        <div class="icons">
+          <ion-icon name="location-outline"></ion-icon>
+          <p class="" ><span v-if="merchant.state === null ">null</span> <span v-else>{{ merchant.city }}, {{ merchant.state }}</span></p>
+        </div>
+        
+        <!-- <h6>Documents</h6>
+        <div class="d-flex" style="gap:20px">
+          <div v-for="doc in merchant.documents" :key="doc.id" >
+          <p class="small"> {{ doc.name }} </p>
+          <div class="verified">Verified</div>
+        </div>
+        </div> -->
+      </div>
+
+      <div class="right__side">
+        <span v-for="doc in merchant.documents" :key="doc.id" > 
+              <img v-if="doc.name === 'Shop Photo' " :src="baseurl + doc.file" alt=""  class="image__show"> </span>
+      </div>
+      </div>
+      </div>
+
+     
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      baseurl: "https://api.verify.szcmerchant.com/",
+      merchant_card: false,
+      dataArr: [],
+      search: "",
+      dataObj: {},
+      documents: [],
+      merchant: {},
+    };
+  },
+  methods: {
+    async getKYC(page = 1) {
+      this.$axios
+        .get("api/kyc", {
+          params: {
+            page,
+          },
+        })
+        .then((res) => {
+          this.dataArr = res.data.kyc.data;
+          this.dataObj = res.data.kyc;
+          console.log(this.dataArr);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    next(page) {
+      this.getKYC(page);
+    },
+    async viewMore(item) {
+      // alert(item.reference_number)
+      const new_id = item.reference_number;
+      try {
+        const res = await this.$axios.get(
+          `${this.baseurl}api/kyc/${new_id}/view`
+        );
+        this.merchant_card = !this.merchant_card
+        console.log(res.data.kyc);
+        this.merchant = res.data.kyc;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  async created() {
+    this.getKYC();
+  },
+};
 </script>
 
 <style>
-.verified_merchant_wrap {
-  padding: 100px;
-}
 .verification_content {
   background-color: #f8f7ff;
+  /* padding: 100px;  */
   /* border-radius: 10px; */
-  padding: 20px;
+  padding: 100px;
 }
 .verification_content .v-data-table--fixed-header > .v-data-table__wrapper {
   background-color: #f8f7ff;
@@ -81,13 +216,125 @@ export default {};
   border: none;
   box-shadow: none;
 }
-
+.image__pp {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: top;
+}
+.image__pp2 {
+  width: 100px;
+  height:100px;
+  border-radius: 50%;
+  margin-bottom: 1rem;
+  object-fit: cover;
+  object-position: top;
+}
+.image__show {
+  width: 100%;
+  /* height: 30; */
+  /* border-radius: 50%; */
+  object-fit: cover;
+  object-position: top;
+}
+.merchant-card-container {
+  background: #01000564;
+  z-index: 999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.merchant__card {
+  width: 30%;
+  background: #fff;
+  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.05);
+  border-radius: 3px;
+  position: relative;
+}
+.merchant-card-container ion-icon {
+  font-weight: bold;
+  font-size: 36px;
+  color: #ff0000;
+}
+.merchant__card .icons ion-icon {
+  font-size: 15px;
+  color: #000;
+}
+.merchant__card .icons {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 10px;
+}
+.merchant__card .icons a{
+  margin: 0;
+  font-size: 15px;
+  
+  color: #000;
+}
+.merchant__card .icons p{
+   margin: 0;
+  font-size: 15px;
+}
+.verified {
+  background-color: #0c680c31;
+  border: 1px solid #0c680c31;
+  border-radius: 3%;
+  /* height: 8rem; */
+  /* width: 8rem; */
+  display: flex;
+  /* width: 30%; */
+  align-items: center;
+  /* justify-content: center; */
+  /* position: absolute;
+  top: 32%;
+  left: 45%; */
+  padding: 0.3rem 0.8rem;
+  gap: 10px
+}
+.verified ion-icon {
+  color: #0c680c;
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+.verified h5{
+  margin: 0;
+  font-size: 0.8rem;
+  font-weight: bold;
+  /* margin-top: -1.3rem; */
+  color: #0c680c;
+}
 @media (max-width: 768px) {
-  .verified_merchant_wrap {
-    padding: 10px;
+  .verification_content {
+    padding: 1.5rem;
   }
+
   .verification_content h1 {
     font-size: 30px;
   }
+  .merchant__card {
+  width: 80%
+}
+.image__pp2 {
+  width: 80px;
+  height:80px;
+  border-radius: 50%;
+  /* margin-bottom: 1rem; */
+  object-fit: cover;
+  object-position: top;
+}
+.image__show {
+  width: 100%;
+  height: 200px;
+  /* border-radius: 50%; */
+  object-fit: cover;
+  object-position: top;
+}
 }
 </style>
